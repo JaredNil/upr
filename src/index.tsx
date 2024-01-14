61ecdc7dfa7d88dbcef9a991b47f1f7f8d333fb0
JaredN <jared.bozh@gmail.com>
2024-01-14 09:35:42 +0800

    stylelint & vscode setting eslint

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from './app/providers/ThemeProvider'
 
 import 'shared/config/i18n/i18n'
 
-const container = document.getElementById('app')
+const container = document.getElementById('root')
 
 const root = createRoot(container)
 
